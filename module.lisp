9d83850cdcd09f9b1a8995acb2b57c1717ddf658
Mariano Montone <marianomontone@gmail.com>
2022-03-19 16:49:42 -0300

    Don't require swank

--- module.lisp
+++ module.lisp
@@ -1,6 +1,6 @@
+;;(require :swank)
 (require :asdf)
 (require :alexandria)
-(require :swank)
 (require :closer-mop)
 
 (load (merge-pathnames #p"def-properties.lisp" (uiop/pathname:pathname-directory-pathname *load-pathname*)))
