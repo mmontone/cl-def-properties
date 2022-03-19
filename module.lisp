389b95944e9fe3b12d90e04c02377298c4953f02
Mariano Montone <marianomontone@gmail.com>
2022-03-19 16:03:15 -0300

    Fix

--- module.lisp
+++ module.lisp
@@ -3,5 +3,4 @@
 (require :swank)
 (require :closer-mop)
 
-(load (merge-pathnames #p"package.lisp" (uiop/pathname:pathname-directory-pathname *load-pathname*)))
 (load (merge-pathnames #p"def-properties.lisp" (uiop/pathname:pathname-directory-pathname *load-pathname*)))
